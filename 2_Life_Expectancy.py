a9004397769f01294004a6af02b936adcff408b2
hubruk <73857309+hubruk@users.noreply.github.com>
2021-10-14 19:01:52 +0200

    Update 2_Life_Expectancy.py
    Implementing Neural Networks
    The World Health Organization (WHO)’s Global Health Observatory (GHO) data repository tracks life expectancy for countries worldwide by following health status and many other related factors.
    
    Although there have been a lot of studies undertaken in the past on factors affecting life expectancy considering demographic variables, income composition, and mortality rates, it was found that the effects of immunization and human development index were not taken into account.
    
    This dataset covers a variety of indicators for all countries from 2000 to 2015 including:
    
    immunization factors
    mortality factors
    economic factors
    social factors
    other health-related factors
    Ideally, this data will eventually inform countries concerning which factors to change in order to improve the life expectancy of their populations. If we can predict life expectancy well given all the factors, this is a good sign that there are some important patterns in the data. Life expectancy is expressed in years, and hence it is a number. This means that in order to build a predictive model one needs to use regression.
    
    In this project, you will design, train, and evaluate a neural network model performing the task of regression to predict the life expectancy of countries using this dataset. Excited? Let’s go!
    
    Load the life_expectancy.csv dataset into a pandas DataFrame.
    Observe the data by printing the first five entries in the DataFrame dataset.
    Drop the Country column from the DataFrame using the DataFrame drop method. Why? To create a predictive model, knowing from which country data comes can be confusing and it is not a column we can generalize over. We want to learn a general pattern for all the countries, and not only those dependent on specific countries.
    In the next two steps, you will split the data into labels and features.
    Features span from the first column to the last column (not including it, since it’s a label column in our dataset). Create a new variable called features.
    When you observed your dataset you probably noticed that some columns are categorical. We learned in this lesson that categorical columns need to be converted into numerical columns.
    Split your data into training set and test sets.
    The next step is to standardize/normalize your numerical features.
    Fit your instance ct of ColumnTransformer to the training data and at the same time transform it.
    Transform your test data instance features_test.
    Create an instance of the tensorflow.keras.models.Sequential model called my_model.
    Create and add the input layer to the network model.
    Add one hidden layer with any number of hidden units you wish.
    Add a output layer with one neuron since you need a single output for a regression prediction.
    Print the summary of the model.
    Create an instance of the Adam optimizer with the learning rate equal to 0.01.
    Once your optimizer is initialized, compile the model. 
    Assign the following parameters:
    For loss use the Mean Squared Error (mse)
    For metrics use the Mean Absolute Error (mae)
    For opt (the optimizer parameters) use the instance of the optimizer you created in the previous step.
    
    Train your model with the fit() method by passing it the following parameters:
    your preprocessed training data
    training labels
    number of epochs equal to 40
    batch size equal to 1
    verbose set to 1
    
    Evaluate your trained model.
    Print your final loss (RMSE) and final metric (MAE) to check the predictive performance on the test set.

--- 2_Life_Expectancy.py
+++ 2_Life_Expectancy.py
@@ -1,11 +1,44 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import Normalizer 
+from sklearn.compose import ColumnTransformer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import InputLayer
+from tensorflow.keras.layers import Dense
+from tensorflow import keras
 
 dataset = pd.read_csv('life_expectancy.csv')
 
 dataset = dataset.drop(['Country'], axis = 1)
-print(dataset.head())
-print(dataset.describe())
+#print(dataset.head())
+#print(dataset.describe())
 
 labels = dataset.iloc[:,-1]
 features= dataset.iloc[:,0:-1]
 
+features = pd.get_dummies(features)
+pd.set_option('display.max_columns', None)
+#print(features.head())
+
+features_train, features_test ,labels_train, labels_test =train_test_split(features, labels, test_size = 0.3, random_state = 500)
+
+ct = ColumnTransformer([("only numeric", Normalizer(),list(features.select_dtypes(include=['float64', 'int64']).columns))], remainder='passthrough')
+#print(features_train)
+
+features_train_scaled = ct.fit_transform(features_train)
+features_test_scaled = ct.transform(features_test)
+
+my_model = Sequential()
+input = InputLayer(input_shape = (features_train_scaled.shape[1], ))
+my_model.add(input)
+my_model.add(Dense(100, activation="relu"))
+my_model.add(Dense(1))
+print(my_model.summary())
+
+opt = keras.optimizers.Adam(learning_rate=0.01)
+my_model.compile(loss='mse', metrics = 'mae', optimizer = opt)
+
+my_model.fit(features_train_scaled, labels_train, epochs = 40, batch_size = 1, verbose = 1)
+
+res_mse, res_mae = my_model.evaluate(features_test_scaled, labels_test, verbose = 0)
+print(res_mse, res_mae)

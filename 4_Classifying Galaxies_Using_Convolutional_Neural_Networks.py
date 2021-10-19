63d03fb912c8b25ba7e5f8835753ff2d9616cf0d
hubruk <73857309+hubruk@users.noreply.github.com>
2021-10-19 16:24:58 +0200

    Update 4_Classifying Galaxies_Using_Convolutional_Neural_Networks.py
    Now, let’s go back and finish fleshing out your architecture.
    
    Train your model.
    
    Now you can run your code to train the model. Training may take a minute or two. After training for twelve epochs, your model’s accuracy should be around 0.60-0.70, and your AUC should fall into the 0.80-0.90 range!
    
    
    BONUS   Want to visualize how your convolutional neural network processes images? Take a look at visualize.py. It contains a function, visualize_activations().

--- 4_Classifying Galaxies_Using_Convolutional_Neural_Networks.py
+++ 4_Classifying Galaxies_Using_Convolutional_Neural_Networks.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from visualize import visualize_activations
 
 from sklearn.model_selection import train_test_split
 from utils import load_galaxy_data
-from tensorflow.keras import activations
 from tensorflow.keras import layers
 
 import app
@@ -19,8 +19,19 @@ input_data_validation_flow = img_gen.flow(input_data_train, labels_train, batch_
 
 model = tf.keras.Sequential()
 model.add(tf.keras.Input(shape = (128, 128, 3)))
-model.add(tf.keras.layers.Dense(4))
-model.add(layers.Activation(activations.softmax))
+model.add(tf.keras.layers.Conv2D(8, 3 ,strides = 2, activation = "relu"))
+model.add(tf.keras.layers.MaxPooling2D(pool_size =(2, 2), strides =(2,2)))
+model.add(tf.keras.layers.Conv2D(8, 3 ,strides = 2, activation = "relu"))
+model.add(tf.keras.layers.MaxPooling2D(pool_size =(2, 2), strides =(2,2)))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(16,activation = 'relu'))
+model.add(tf.keras.layers.Dense(4,activation = 'softmax'))
 
 model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001), loss = tf.keras.losses.CategoricalCrossentropy(), metrics = [tf.keras.metrics.CategoricalAccuracy(),tf.keras.metrics.AUC()])
+model.summary()
 
+hist = model.fit(input_data_train_flow, steps_per_epoch = len(input_data_train)/5,epochs = 12, validation_data = input_data_validation_flow, validation_steps = len(input_data_validation_flow)/5)
+
+visualize_activations(model, input_data_validation_flow)
+
+print(hist.history)
